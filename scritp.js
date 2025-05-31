a963a25917105661c5af18779b26349de9a16373
Sebastián Ignacio Pachas Negrón <121914202+SebastianPachas12@users.noreply.github.com>
2025-05-31 13:08:52 -0500

    Create scritp.js

---
+++ scritp.js
@@ -0,0 +1,6 @@
+let nombre = prompt("¿Como te llamas?")
+let apellido = prompt("¿Como te apellidas?")
+let edad = prompt("¿Cual es tu edad?")
+
+alert(`Bienvenido ${nombre} ${apellido}, tienes ${edad} años de edad`)
+console.log(`Hola ${nombre} ${apellido}`)
